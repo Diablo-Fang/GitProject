38801c9f41516e8974b69bf7e8558dc4a42cef5c
Diablo-Fang <116329466+Diablo-Fang@users.noreply.github.com>
2024-05-02 21:51:05 +0800

    Add files via upload

---
+++ GitProject/index.java
@@ -0,0 +1,11 @@
+package console;
+
+import java.util.Scanner;
+
+public class index {
+    public final static Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("fang±àĞ´µÄ´úÂë");
+    }
+}
